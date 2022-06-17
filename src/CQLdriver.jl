8241f2e7e41dcedd6e8e1cc1163d7fe25ce016a5
Adrien ASESIO <aasesio@gmail.com>
2022-06-17 15:29:50 -0400

    Removed JuliaDB

--- src/CQLdriver.jl
+++ src/CQLdriver.jl
@@ -1,7 +1,7 @@
 __precompile__(true)
 
 module CQLdriver
-using DataFrames, Dates, StructArrays, IndexedTables, JuliaDB
+using DataFrames, Dates, StructArrays, IndexedTables
 using UUIDs: UUID
 
 export DataFrames, cqlinit, cqlclose, cqlwrite, cqlread, cqlexec, cql_uuid_gen_new, cql_uuid_gen_free, cql_uuid_gen_random
@@ -713,14 +713,6 @@ function cqlwrite(s::Ptr{CassSession}, cass_table::String, data::Union{DataFrame
         end
     end
     return err::UInt16
-end
-
-function cqlwrite(s::Ptr{CassSession}, cass_table::String, data::JuliaDB.IndexedTable; paritionsize::Int=100000, batchsize::Int=500, retries::Int=5, counter::Bool=false)
-    errs = Vector{UInt16}()
-    for tbl = Iterators.partition(data, paritionsize)
-        push!(errs, cqlwrite(s, cass_table, tbl; batchsize=batchsize, retries=retries, counter=counter))
-    end
-    return union(errs)[1]
 end
 
 """
